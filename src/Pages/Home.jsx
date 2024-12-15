efdcf14702c4c60a4210c4c0464f42360bfa2469
developerputul <putulsentongchangya7@gmail.com>
2024-12-15 12:50:10 +0600

    Grid Slider section Updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroBanner from '../components/HeroBanner'
+import MultiBanner from '../components/MultiBanner'
 
 const Home = () => {
   return (
     <>
       <HeroBanner/>
+      <MultiBanner/>
     </>
   )
 }
