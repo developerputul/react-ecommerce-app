bb66f6fb1086ff6f190eb845554d59f9ef7b81b0
developerputul <putulsentongchangya7@gmail.com>
2024-12-23 19:47:49 +0600

    Completed in the Project

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,7 +18,9 @@ const Navbar = () => {
   return (
     <div className="bg-white px-4 fixed w-full z-50 shadow-sm top-0 shadow-gray-400">
       <div className="max-w-7xl mx-auto py-2 px-5 flex justify-between items-center">
-        <img src={Logo} alt="Logo" className="md:w-24 w-20" />
+
+        <Link to={'/'}><img src={Logo} alt="Logo" className="md:w-24 w-20" /></Link>
+       
         <div className="flex items-center gap-5">
           <nav className="hidden md:block">
             <ul className="flex items-center font-semibold text-xl gap-7">
